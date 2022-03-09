e565bee5bdc5002c227d5272314d5638e68dc6a1
Juan Visney <jv575juan@gmail.com>
2022-03-09 19:19:31 -0400

    prueba1

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,13 @@
 const env = require('dotenv')
 env.config()
 
-console.log(process.env.DATABASE_PORT)
-console.log(process.env.DATABASE_USER)
-console.log(process.env.DATABASE_PASSWORD)
-console.log(process.env.DATABASE_NAME)
-
 module.exports = {
     database: {
-        host: process.env.DATABASE_PORT || 'localhost',
+        host: process.env.DATABASE_HOST || 'localhost',
         user: process.env.DATABASE_USER || 'root',
         password: process.env.DATABASE_PASSWORD || 'jvcode',
-        database: process.env.DATABASE_NAME || 'ejemplo'
+        database: process.env.DATABASE_NAME || 'ejemplo',
+        port: process.env.DATABASE_PORT
     },
     port: process.env.PORT || 3000
 }
